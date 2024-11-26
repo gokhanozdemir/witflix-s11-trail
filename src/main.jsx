b5b4767a50b16759f6166232f3d202b3032f100c
Gökhan <gn.ozdemir@gmail.com>
2024-11-26 11:32:40 +0300

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import App from './App.jsx';
-import ProvideAuth from './contexts/AuthContext.jsx';
+import { ProvideAuth } from './contexts/AuthContext.jsx';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
